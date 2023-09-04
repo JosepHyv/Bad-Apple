d4d0e6761bcc47256610180547767c92ec6543c4
josephyv <haguilar@auronix.com>
2023-09-04 10:00:28 -0600

    adding ejecution script

---
+++ start.py
@@ -0,0 +1,11 @@
+from bad_apple.ascii_video import start_bad_apple
+from bad_apple.video_data import generate_input_data
+
+
+def main():
+    generate_input_data()
+    start_bad_apple()
+
+
+if __name__ == '__main__':
+    main()
